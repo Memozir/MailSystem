df81a5b4880cdc4e9a3df964a813a60790433af1
Memozir <gizmovap@gmail.com>
2023-11-06 15:33:16 +0300

    load env changes; added get user by id db function and http handler

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,25 @@ import (
 	"mail_system/internal/db"
 	"mail_system/internal/handlers"
 	"mail_system/internal/server"
-	utils "mail_system/internal/utils"
+	"mail_system/internal/utils"
 	"os"
+	"os/signal"
+	"syscall"
 )
+
+func graceFullShutdown(ch chan os.Signal, db db.Storage) {
+	for {
+		select {
+		case <-ch:
+			close(ch)
+			db.Reset()
+			log.Fatalf("APPLICATION WAS GRACEFULLY SHUTTED DOWN!")
+			return
+		default:
+			continue
+		}
+	}
+}
 
 func main() {
 	utils.LoadEnv()
@@ -20,11 +36,16 @@ func main() {
 	serverPort := os.Getenv("SERVER_PORT")
 	server, err := server.NewServer(serverHost, serverPort, db)
 
-	if err != nil {
-		log.Fatal("Server have not strted")
+	if server == nil || err != nil {
+		log.Fatal("Server have not started")
 	}
 
 	mailHandler := handlers.NewMailHandler(db)
 	mux := mailHandler.LoadHandlers()
+
+	exit := make(chan os.Signal, 1)
+	go graceFullShutdown(exit, db)
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+
 	server.Start(mux)
 }
