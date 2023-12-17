8a9e6fd638e00bb296caaad01f59b704ab85ca86
Memozir <gizmovap@gmail.com>
2023-12-17 23:50:27 +0300

    Fix address splitting

--- internal/db/migrations/20231216165531_client_add_apartment.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE "client" ADD COLUMN apartment varchar(30) not null default '';
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE "client" DROP COLUMN apartment;
--- +goose StatementEnd
